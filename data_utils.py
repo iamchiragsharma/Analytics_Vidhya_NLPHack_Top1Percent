765cd481858f822120b4b60baa61fc26d653a75c
iamchiragsharma <chiragsharmagtbit@outlook.com>
2020-04-25 23:33:40 +0530

    Roughly Added Everything

---
+++ data_utils.py
@@ -0,0 +1,24 @@
+import unicodedata
+import re
+from contractions_dict import CONTRACTION_MAP
+
+def remove_accented_chars(text):
+    text = unicodedata.normalize('NFKD', text).encode('ascii','ignore').decode('utf-8', 'ignore')
+    return text
+
+
+def strict_remove_accented_chars(text):
+    text = un
+
+
+def expand_contractions(text, contraction_mapping=CONTRACTION_MAP):
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match) if contraction_mapping.get(match) else contraction_mapping.get(match.lower())
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
